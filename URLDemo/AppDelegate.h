c54f6bba9b5333cff07f4dcd66ff9b2bd065b4bf
nate <nate@nate.lan>
2015-07-27 20:46:53 +0800

    Initial Commit

---
+++ URLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  URLDemo
+//
+//  Created by nate on 15/7/27.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ URLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  URLDemo
+//
+//  Created by nate on 15/7/27.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
